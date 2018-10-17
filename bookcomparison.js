652eb9b820ddad69c0d8391281f5d43c66a82b77
Isabel Hilton <hiltonisabelc@gmail.com>
2018-10-17 05:09:39 +0000

    Formatting

--- bookcomparison.js
+++ bookcomparison.js
@@ -13,7 +13,9 @@ function mainCtrl($scope, $http) {
 
     $scope.addNew = function(book) {
         console.log(book);
-        var url = "https://www.goodreads.com/search.xml?key=xqIBQHVgUlJIE5jIX8GTg&q=Ender%27s+Game"; // + form;
+        //var url = "https://www.goodreads.com/search.xml?key=xqIBQHVgUlJIE5jIX8GTg&q=Ender%27s+Game"; // + form;
+        var url = "https://www.goodreads.com/book/title.json?key=xqIBQHVgUlJIE5jIX8GTg&title=Ender%27s+Game";
+        //var url = "http://openlibrary.org/query.json?type=/type/edition&title=Ender%27s+Game";
         console.log(url);
         $http.jsonp(url,{jsonpCallbackParam: 'callback'})
         .then(function(response) {
@@ -46,8 +48,14 @@ function comparisonDirective() {
         replace: 'true',
         template: (
             '<div class="Comparison">' +
+            '<div class="row">' +
+            '<div class="col-md-6">' +
             '<h4>First book: {{book.first}}</h4>' +
+            '</div>' +
+            '<div class="col-md-6">' +
             '<h4>Second book: {{book.second}}</h4>' +
+            '</div>' +
+            '</div>' +
             '</div>'
         ) //,
         //link: link
