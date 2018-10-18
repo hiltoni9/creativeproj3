acf3e8ac921062a752f0f12d6803e3bd7c6096c1
Isabel Hilton <hiltonisabelc@gmail.com>
2018-10-18 19:09:53 +0000

    trying api urls

--- bookcomparison.js
+++ bookcomparison.js
@@ -14,14 +14,17 @@ function mainCtrl($scope, $http) {
     $scope.addNew = function(book) {
         console.log(book);
         //var url = "https://www.goodreads.com/search.xml?key=xqIBQHVgUlJIE5jIX8GTg&q=Ender%27s+Game"; // + form;
-        var url = "https://www.goodreads.com/book/title.json?key=xqIBQHVgUlJIE5jIX8GTg&title=Ender%27s+Game";
+        //var url = "https://www.goodreads.com/book/title.json?key=xqIBQHVgUlJIE5jIX8GTg&title=Ender%27s+Game";
         //var url = "http://openlibrary.org/query.json?type=/type/edition&title=Ender%27s+Game";
-        console.log(url);
+        //var url = "http://api.nytimes.com/svc/books/v3/reviews.jsonp?api-key=602f6fba4a6f4fe2aacb858305719e52&title=1984";
+        var url = "https://api.nytimes.com/svc/books/v3/reviews.json?api-key=602f6fba4a6f4fe2aacb858305719e52&title=1984";
+        //console.log(url);
         $http.jsonp(url,{jsonpCallbackParam: 'callback'})
         .then(function(response) {
             console.log(response);
             $scope.bookData = response.data;
         });
+        console.log(url);
         /*url = "https://api.github.com/users/mjcleme";
         $http.get(url).then(function(response) {
             console.log(response);
